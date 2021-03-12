d5737944c22e5ce842765a256b41dfeb2d3cd613
shw1606 <shw4008@gmail.com>
2021-03-12 21:30:51 +0900

    5: 그냥 npm, publish

--- index.js
+++ index.js
@@ -1,13 +1,3 @@
-const { odd, even } = require("./var");
-const checkOddOrEven = require("./func");
-
-function checkStringOddOrEven(str) {
-  if (str.length % 2) {
-    return odd;
-  }
-  return even;
-}
-
-console.log(checkOddOrEven(10));
-console.log(checkStringOddOrEven("hell"));
-console.log(__dirname);
+module.exports = () => {
+  return "hello package";
+};
